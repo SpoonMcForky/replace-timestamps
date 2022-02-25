427d19558537ddca5814931201d0e78c8d4be5cb
SpoonMcForky <55011606+SpoonMcForky@users.noreply.github.com>
2022-02-25 06:03:44 -0500

    Use Astra modules

--- index.tsx
+++ index.tsx
@@ -1,25 +1,23 @@
 import { UPlugin } from '@classes';
-import { getByDisplayName, React } from '@webpack';
 const Patcher = require('@patcher');
-const { inject, uninject } = require('powercord/injector');
-const { messages } = require('powercord/webpack');
+const { MessageActions } = require('@webpack');
 
 export default class resize extends UPlugin {
   start() {
     this.patchMessage();
   }
   patchMessage() {
-    inject('injection', messages, 'sendMessage', args => {
-      if (args[1].content.search(/\d\d:\d\d/) !== -1) args[1].content = args[1].content.replace(/\d\d:\d\d/, this.getUnixTimestamp(args[1].content.match(/\d\d:\d\d/)));
-      return args;
-    }, true);
+    Patcher.before('patch', MessageActions, 'sendMessage', (_, ret) => {
+      if (ret[1].content.search(/\d\d:\d\d/) !== -1) ret[1].content = ret[1].content.replace(/\d\d:\d\d/, this.getUnixTimestamp(ret[1].content.match(/\d\d:\d\d/)));
+      return ret;
+    });
   }
   getUnixTimestamp(time) {
-    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/\d\d:\d\d/, time)
+    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/\d\d:\d\d/, time);
     const then = Math.round((new Date(date)).getTime() / 1000);
-    return `<t:${then}:t>`;
+    return `<t:${then}:t>`; //To change the time format, refer to https://github.com/discord/discord-api-docs/blob/master/docs/Reference.md#timestamp-styles
   }
   stop() {
-    uninject('injection');
+    Patcher.unpatchAll('patch');
   }
 }
