d8acdf88d242af9277c1311c919cd3cb95880c9c
SpoonMcForky <55011606+SpoonMcForky@users.noreply.github.com>
2022-02-25 06:30:31 -0500

    Initial Commit

--- index.tsx
+++ index.tsx
@@ -1,23 +1,23 @@
-import { UPlugin } from '@classes';
-const Patcher = require('@patcher');
-const { MessageActions } = require('@webpack');
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { messages } = require('powercord/webpack');
 
-export default class resize extends UPlugin {
-  start() {
+export default class timestamp extends Plugin {
+  startPlugin() {
     this.patchMessage();
   }
   patchMessage() {
-    Patcher.before('patch', MessageActions, 'sendMessage', (_, ret) => {
-      if (ret[1].content.search(/\d\d:\d\d/) !== -1) ret[1].content = ret[1].content.replace(/\d\d:\d\d/, this.getUnixTimestamp(ret[1].content.match(/\d\d:\d\d/)));
-      return ret;
-    });
+    inject('injection', messages, 'sendMessage', args => {
+      if (args[1].content.search(/\d\d:\d\d/) !== -1) args[1].content = args[1].content.replace(/\d\d:\d\d/, this.getUnixTimestamp(args[1].content.match(/\d\d:\d\d/)));
+      return args;
+    }, true);
   }
   getUnixTimestamp(time) {
     const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/\d\d:\d\d/, time);
     const then = Math.round((new Date(date)).getTime() / 1000);
     return `<t:${then}:t>`; //To change the time format, refer to https://github.com/discord/discord-api-docs/blob/master/docs/Reference.md#timestamp-styles
   }
-  stop() {
-    Patcher.unpatchAll('patch');
+  stopPlugin() {
+    uninject('injection');
   }
 }
